b96e7a5e678140d74b11a78304e1679d263cec50
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-20 17:47:41 +0200

    finished video 6 from module 2

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View >
+    <View style ={{padding:50}} >
       <View>
-
+        <TextInput placeholder ="Course Goals" style ={{
+          borderBottomColor:'black',
+          borderBottomWidth:1,
+        }}/>
+        <Button title="ADD" />
       </View>
       <View >
-        
+
       </View>
     </View>
   );
