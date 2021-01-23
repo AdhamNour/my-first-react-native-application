6505db96d191759d3e956c5b0102597a10d43859
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-24 01:47:20 +0200

    finished video 8 from module 2

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style ={{padding:50}} >
-      <View style ={{flexDirection: 'row',justifyContent: 'space-between',alignItems: 'center'}}>
-        <TextInput placeholder ="Course Goals" style ={{
-          borderColor:'black',
-          borderWidth:1,
-          padding:10,
-          width: '80%'
-        }}/>
-        <Button title="ADD" />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "85%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          flex:1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View >
-
+      <View
+        style={{
+          backgroundColor: "blue",
+          flex:1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          flex:1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
 }
-
-
